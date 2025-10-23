6c7c861b9eff702d43c3e3ac316d01a0bd7bb7a9
jeevantenny <jeevantenny15@gmail.com>
2025-10-23 18:27:42 +0100

    fixed a crash bug

--- states/init_state.py
+++ states/init_state.py
@@ -10,8 +10,6 @@ from .menus import TitleScreen, PauseMenu
 from .play import Play
 from .visuals import BackgroundTint
 
-from .test import TestState
-
 
 
 
@@ -19,9 +17,6 @@ class Initializer:
     "This state will not be added to the state_stack. Instead it adds the relevant states needed when the game is opened."
 
     def __init__(self, state_stack: StateStack):
-        if Cheats.test_state:
-            TestState().add_to_stack(state_stack)
-            return
         try:
             save_data = data.load_progress()
         except SaveFileError as e:
